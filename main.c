2bc8887c9c4de9d7aa6ec49a857d3affea521e61
jantug651-byte <jantug651@gmail.com>
2026-06-13 15:13:38 +0300

    KAN-4

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
 void welcomefunc();
+void printname();
 int main(){
   
 welcomefunc();
 char name[50];
 printf("\nEnter Name: ");
 scanf("%s", name);
+printname(name);
   
 return 0;
 }
 void welcomefunc(){
 printf("Welcome HW-2 Cloud");
+}
+
+void printname(char name[]){
+printf("\nName: %s",name);  
 }
